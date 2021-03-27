8e174cc0153160f0a67bf2d815eff0c63bed3512
Casey D <caseydasilva@hotmail.com>
2021-03-27 13:56:22 -0700

    JS class and file/folder selection
    creating functionality to filter through folders and then files to identify any test files (which all end in the '.test.js' notation)

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
 
-console.log('hello');
+const Runner = require('./runner');
+const runner = new Runner();
+
+const run = async () => {
+    await runner.collectFiles(process.cwd());
+    console.log(runner.testFiles);
+
+};
+
+run();
