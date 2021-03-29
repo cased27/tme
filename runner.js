b9cc8b246f54dce68c91f54ff7b1ce2ad9ac6b40
Casey D <caseydasilva@hotmail.com>
2021-03-29 12:39:15 -0700

    test/sample projects to test the test
    using jsdom to allow 'browser-type' testing within a node framework

--- runner.js
+++ runner.js
@@ -1,10 +1,40 @@
+
 const fs = require('fs');
 const path = require('path');
+const chalk = require('chalk');
+
+const forbiddenDirs = ['node_modules'];
 
 class Runner {
     constructor() {
         this.testFiles = [];
 
+    }
+
+    async runTests() {
+        for (let file of this.testFiles) {
+            console.log(chalk.gray(`--- ${file.name}---`));
+            const beforeEaches = [];
+            global.beforeEach = (fn) => {
+                beforeEaches.push(fn);
+            }
+            global.it = (desc, fn) => {
+                beforeEaches.forEach(func => func());
+                try {
+                    fn();
+                    console.log(chalk.green(`\tOK - ${desc}`));
+                } catch (err) {
+                    const message = err.message.replace(/\n/g, '\n\t\t')
+                    console.log(chalk.red(`\tX - ${desc}`));
+                    console.log(chalk.red('\t', message));
+               }
+            };
+            try {
+                require(file.name);
+            } catch (err) {
+                console.log(chalk.red(err));
+            }
+        }
     }
 
     async collectFiles(targetPath) {
@@ -15,7 +45,7 @@ class Runner {
 
             if (stats.isFile() && file.includes('.test.js')) {
                 this.testFiles.push({ name: filepath });
-            } else if (stats.isDirectory()) {
+            } else if (stats.isDirectory() && !forbiddenDirs.includes(file)) {
                 const childFiles = await fs.promises.readdir(filepath);
                 files.push(...childFiles.map(f => path.join(file, f)));
             }
